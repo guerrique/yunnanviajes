ed38fde6f1c9d4e8f7eac735b4a7e0b8f2e42be5
guerrique desmoulins guerrique@gmail.com <guerrique@gmail.com>
2019-09-24 11:43:41 +0800

    home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def yunnan
+  end
+
+  def our_tours
+  end
+
+  def about
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'yunnan', to: 'pages#yunnan', as: :yunnan
+  get 'our_tours', to: 'pages#our_tours', as: :our_tours
+  get 'about', to: 'pages#about'
 end
+

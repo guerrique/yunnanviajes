92dec6db03ba25f6fc1c10d5a6daab1465369a8c
guerrique desmoulins guerrique@gmail.com <guerrique@gmail.com>
2019-11-24 16:32:35 +0800

    spanish version

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,10 @@ Rails.application.routes.draw do
   get 'our_tours', to: 'pages#our_tours', as: :our_tours
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
+  get 'home_spanish', to: 'pages#home_spanish', as: :home
+  get 'our_tours_spanish', to: 'pages#our_tours_spanish'
+  get 'yunnan_spanish', to: 'pages#yunnan_spanish'
+  get 'quien_somos', to: 'pages#quien_somos'
+  get 'contacto', to: 'pages#contacto'
 end
 

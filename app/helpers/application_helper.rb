590cb0add7f23036e72c31b4998c926ffebb74a8
guerrique desmoulins guerrique@gmail.com <guerrique@gmail.com>
2019-11-25 15:43:35 +0800

    helper for navbar active links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def nav_bar
+    content_tag(:ul, class: "navbar-nav mr-auto") do
+      yield
+    end
+  end
+
+  def nav_link(text, path, classs)
+    options = current_page?(path) ? { class: "nav-item active" } : { class: "nav-item" }
+    content_tag(:li,  options) do
+      link_to text, path, classs
+    end
+  end
 end

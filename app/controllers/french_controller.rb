f6696926c325a05141453c6183d84899a0eb49bb
guerrique desmoulins guerrique@gmail.com <guerrique@gmail.com>
2019-12-07 19:24:31 +0100

    versions + traduction français page yunnan

---
+++ app/controllers/french_controller.rb
@@ -0,0 +1,7 @@
+class FrenchController < ApplicationController
+  def accueil
+  end
+
+  def a_propos
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,24 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  root to: 'spanish#casa'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'home_en', to: 'pages#home_en'
   get 'yunnan', to: 'pages#yunnan', as: :yunnan
   get 'our_tours', to: 'pages#our_tours', as: :our_tours
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
-  get 'home_spanish', to: 'pages#home_spanish', as: :home
-  get 'our_tours_spanish', to: 'pages#our_tours_spanish'
-  get 'yunnan_spanish', to: 'pages#yunnan_spanish'
-  get 'quien_somos', to: 'pages#quien_somos'
-  get 'contacto', to: 'pages#contacto'
+
+  #spanish
+  get 'casa', to: 'spanish#casa', as: :home
+  get 'yunnan_es', to: 'spanish#yunnan_es'
+  get 'nuestro_tours', to: 'spanish#nuestro_tours'
+  get 'quien_somos', to: 'spanish#quien_somos'
+  get 'contacto', to: 'spanish#contacto'
+
+  #french
+  get 'accueil', to: 'french#accueil'
+  get 'yunnan_fr', to: 'french#yunnan_fr'
+  get 'circuits', to: 'french#circuits'
+  get 'a_propos', to: 'french#apropos'
+  get 'contact_fr', to: 'french#contact_fr'
 end
 

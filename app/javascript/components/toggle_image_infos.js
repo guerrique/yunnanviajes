00118b356a97f74a489b4724fcc455b01510aa18
guerrique desmoulins guerrique@gmail.com <guerrique@gmail.com>
2020-01-19 16:29:00 +0800

    get rid of script tags to put all javascript code in javascript folder

--- app/javascript/components/toggle_image_infos.js
+++ app/javascript/components/toggle_image_infos.js
@@ -1,6 +1,32 @@
 const toggleImageInfos = () => {
-  console.log('Helle from toggle_truc');
+  console.log('Hello from toggle_truc');
 }
 
+  function showParagraph(e) {
+
+    // location = e.currentTarget.dataset.location
+    // targetElement = document.querySelector(`.cardtext[data-location='${location}']`);
+
+    console.log(e.currentTarget);
+    console.log(e.currentTarget.id);
+    // displays description text of card clicked
+
+    console.log(`${e.currentTarget.id}_description`);
+
+    const description = document.getElementById(`${e.currentTarget.id}_description`);
+    if(description.style.display === "none") {
+      // hides the description of the card previously shown
+      const descOther = document.querySelectorAll(".description");
+      // console.log(descOther);
+      descOther.forEach(d => d.style.display = "none");
+      description.style.display = "block";
+      description.scrollIntoView();
+    } else {
+      description.style.display = "none";
+    }
+  }
+
+  const card = document.querySelectorAll('.card');
+  card.forEach(c => c.addEventListener('click', event => showParagraph(event)));
 
 export { toggleImageInfos };
